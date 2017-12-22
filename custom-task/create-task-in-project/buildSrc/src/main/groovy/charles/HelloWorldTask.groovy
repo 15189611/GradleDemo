2714b095a6da4df7f45c4b099061c98ee8f40fde
17111980 <panlia@cnsuning.com>
2017-12-22 17:34:11 +0800

    添加gradle

---
+++ custom-task/create-task-in-project/buildSrc/src/main/groovy/charles/HelloWorldTask.groovy
@@ -0,0 +1,15 @@
+package charles
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+
+class HelloWorldTask extends DefaultTask {
+
+   @Optional
+    String message = 'I am davenkin'
+
+    @TaskAction
+    def hello(){
+        println "hello world $message"
+    }
+
+}
